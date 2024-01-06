f4b974efad17c27a389fe95d4c58a5ec107bba20
Адина <adina.duishobaeva.ch@gmail.com>
2024-01-06 11:55:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // ЗАДАЧА
+
+        // МАССИВТИН ЭН АКЫРКЫ ЭЛЕМЕНТИН АЛУУ
+
+        int[]array= new int[]{1,2,3,454,5656,77,9898,989,5678,98,89};
+        System.out.println(" Массивтин эн чон элементи :  "  +  array[array.length - 1]);
+        System.out.println("Массивтин ортонку элементи  "   +  array[array.length - 5]);
+
+    }
+}
